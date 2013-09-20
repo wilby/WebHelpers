81d24a520331bc56878ef494bbffaa14524a231d
Wilby Jackson <jacksw02@gmail.com>
2013-09-19 20:00:50 -0400

    Added solution to src folder

---
+++ src/WebHelpers/StackExchange/DataExplorer/Helpers/DataKeys.cs
@@ -0,0 +1,13 @@
+﻿﻿namespace StackExchange.DataExplorer.Helpers
+{
+    /// <summary>
+    /// Contains keys for common app-wide keys (used in querystring, form, etc)
+    /// </summary>
+    public static class Keys
+    {
+        public const string OpenId = "openid_identifier";
+        public const string Session = "s";
+        public const string ReturnUrl = "returnurl";
+        public const string UserFlag = "m";
+    }
+}
